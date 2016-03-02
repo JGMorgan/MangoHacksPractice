799e3e2098c8e03a13153c84c60e096775a7630b
leonliang33 <llian008@fiu.edu>
2016-03-02 17:26:29 -0500

    testing

--- authorization_code/color_main.js
+++ authorization_code/color_main.js
@@ -1,4 +1,18 @@
-function getColor() {
-    this.canvas = document.createElement('canvas');
+function getColor(canvas) {
+    var selectIMG = document.getElementById(canvas);
+    var context = selectIMG.getContext("2d");
+    function readImage() {
+        if (this.files && this.files[0]) {
+            var FR = new FileReader();
+            FR.onload = function (e) {
+                var img = new Image();
+                img.onload = function () {
+                    context.drawImage(img, 0, 0);
+                };
+                img.src = e.target.result;
+            };
+            FR.readAsDataURL(this.files[0]);
+        }
+    }
 
 }
